90a7af56be643c7257e8bd0d0f811eb791340b3c
Death0bomb <sousa-10-98@hotmail.com>
2022-06-24 01:47:09 +0100

    Sign Up e Login
    Funcionalidade de Sign up e Login Funcional
    Instalação de Firebase na app juntamente como as dependencias

--- app/src/main/java/com/example/aplicaodechat/SignUp.kt
+++ app/src/main/java/com/example/aplicaodechat/SignUp.kt
@@ -1,11 +1,60 @@
 package com.example.aplicaodechat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUp : AppCompatActivity() {
+
+    private lateinit var edtName: EditText
+    private lateinit var  edtEmail: EditText
+    private lateinit var  edtPassword: EditText
+    private lateinit var  btnSignUp: Button
+
+    private lateinit var mAuth: FirebaseAuth
+
+
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        supportActionBar?.hide()
+
+        edtName = findViewById(R.id.edt_name)
+        edtEmail = findViewById(R.id.edt_email)
+        edtPassword = findViewById(R.id.edt_password)
+        btnSignUp = findViewById(R.id.btnSignUp)
+
+        mAuth = FirebaseAuth.getInstance()
+
+        btnSignUp.setOnClickListener {
+            val email = edtEmail.text.toString()
+            val password = edtPassword.text.toString()
+
+            signUp(email,password)
+        }
+    }
+
+    private fun signUp(email: String, password: String){
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+
+                    val intent = Intent(this@SignUp, MainActivity::class.java)
+                    startActivity(intent)
+
+                } else {
+
+                    Toast.makeText(this@SignUp, "Error Ocurred", Toast.LENGTH_SHORT).show()
+
+                }
+            }
     }
 }
